ef6538a9ce2c4e23fc1f08277a2db30eb6b1602d
sitek <sitek.kevin@gmail.com>
2017-09-04 14:57:13 -0700

    fixed error by changing to pandas

--- demean_age.py
+++ demean_age.py
@@ -1,7 +1,8 @@
 import sys
 import numpy as np
+import pandas as pd
 
-age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
+age = pd.load_csv(sys.argv[1], sep='/t').age
 
 mean_age = sum(age)/len(age)
 assert mean_age < 100
