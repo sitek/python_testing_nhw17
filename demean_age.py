be0595c89cab3ce970c569651a58235168fa55fe
sitek <sitek.kevin@gmail.com>
2017-09-04 14:52:30 -0700

    added assertions

--- demean_age.py
+++ demean_age.py
@@ -4,6 +4,8 @@ import numpy as np
 age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
 
 mean_age = sum(age)/len(age)
+assert mean_age < 100
+assert mean_age > 10
 
 np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
 
